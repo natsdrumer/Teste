88ceabdc668732794ae43d6afef5e37f5fc5c484
natsdrumer <natslevy@gmail.com>
2023-12-14 21:58:11 -0100

    Second Commit

--- app/src/main/java/com/example/teste/RegisterActivity.java
+++ app/src/main/java/com/example/teste/RegisterActivity.java
@@ -3,12 +3,22 @@ package com.example.teste;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText etname, etnumber, etemail, etpass, date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etname = findViewById(R.id.eName);
+        etnumber = findViewById(R.id.eNumber);
+        etemail = findViewById(R.id.eEmail);
+        etpass = findViewById(R.id.ePass);
+        date = findViewById(R.id.eDate);
+
     }
 }
